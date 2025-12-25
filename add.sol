04072c251fb591fbe1b6c8e236cd79779768ce02
yashu780 <sharmayashu74@gmail.com>
2025-12-25 19:56:38 +0530

    Auto update from Remix

---
+++ add.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+contract functionIntro{
+    function add(uint x, uint y) external pure returns(uint){
+        return x+y;
+    }
+}
