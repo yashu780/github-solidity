3f9af739a7ae1033e7eb6fe5f60eb9bb087eb76f
yashu780 <sharmayashu740@gmail.com>
2026-01-06 18:25:13 +0530

    Update Solidity practice files

---
+++ mapping/new1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.10;
+contract Mapping1st{
+    mapping(address=>uint) public balances;
+    mapping(address=>mapping(address=> bool)) public isFriend;
+    function examples() external{
+        balances[msg.sender]=123;
+        uint bal=balances[msg.sender];
+        uint bal2 =balances[address(1)];
+        
+        balances[msg.sender]+=456;
+        delete balances[msg.sender];
+
+
+    }
+}
