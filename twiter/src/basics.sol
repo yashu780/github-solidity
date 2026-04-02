9349aa348e9fe60c038c49c642fd6bbeccedaf44
yashu780 <sharmayashu740@gmail.com>
2026-04-02 07:19:57 +0530

    Update Solidity practice files

--- twiter/src/basics.sol
+++ twiter/src/basics.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.10;
  contract twitter{
-    uint16 constant MAX_TWEET_LENGTH = 280;
+    uint16  public MAX_TWEET_LENGTH = 280;
     struct Tweet{
+        uint256 id;
         address author;
         string content;
         uint256 timestamp;
@@ -10,10 +11,22 @@ pragma solidity ^0.8.10;
         uint256 dislike;
     }
     mapping(address => Tweet[]) public tweets;
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner,"YOU ARE NOT THE OWNER");
+        _;
+    }
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner{
+        MAX_TWEET_LENGTH = newTweetLength;
+    }
     function createTweets(string memory _tweet) public{
          require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "tweet se too long blockchain me paisa lgta hai!" );
        Tweet memory newTweet = Tweet({
-       
+       id: tweets[msg.sender].length,
         author: msg.sender,
         content: _tweet,
         timestamp:block.timestamp,
@@ -22,6 +35,17 @@ pragma solidity ^0.8.10;
        });
         tweets[msg.sender].push(newTweet);
     }
+    function likeTweet(address author,uint256 id) external{
+        require(tweets[author][id].id ==id,"Tweet does not exist");
+        tweets[author][id].likes++;
+
+    }
+    function dislikeTweet(address author,uint256 id) external {
+        require(tweets[author][id].id ==id,"Tweet does not exist");
+        require(tweets[author][id].likes>0,"Tweet has no likes");
+        tweets[author][id].likes--;
+    }
+
     function getTweet(uint256 _i)public view returns(Tweet memory ){
         return tweets[msg.sender][_i];
     }
